f88a72cc08789445b0f71a6de58d3b9b5886dbda
chipsergik <chipsergik@gmail.com>
2015-12-22 00:25:34 +0300

    dkst69 layout
    Сверстать страницы сайта согласно файлу дизайна

---
+++ src/js/reports.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $('.report-periods-radios input').on('change', function (e) {
+        e.preventDefault();
+
+        $('.report-period').hide();
+        var periodId = $(this).val();
+        $('#' + periodId).show();
+    })
+});
